48e5f306cf4a91a2f36cf0d5434300a4521f4118
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2021-08-16 21:29:30 -0500

    feat: agregue archivo impresion-pantalla

---
+++ Dia 1/02-impresion-pantalla.py
@@ -0,0 +1,18 @@
+mes = "agosto"
+
+print("el mes es:", mes)
+
+dia = 16
+
+print("el dia", dia, "del mes", mes)
+
+alumnos = 23
+grupo = 9
+
+print("los alumnos son {} y son del grupo {} y todos estan atentos".format(
+    alumnos, grupo))
+
+print("los alumnos son {1} y son del grupo {0} y todos estan atentos".format(
+    grupo, alumnos))
+
+print(f"los alumnos son {alumnos}")
