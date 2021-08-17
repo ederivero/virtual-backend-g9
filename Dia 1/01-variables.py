001f36f0cb8b140250c5be51d4ba446d53815ceb
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2021-08-16 21:21:36 -0500

    feat: agregue el archivo variables.py

---
+++ Dia 1/01-variables.py
@@ -0,0 +1,23 @@
+# Esto es un comentario
+
+# variables de texto
+nombre = "eduardo"
+apellido = 'de rivero'
+
+direccion = "calle o'higgins 125"
+
+texto = """hola amigos:
+buenas noches, les saluda su profe
+            atentamente,
+                    eduardo"""
+
+texto2 = '''hola amigos:
+ya casi viene el break'''
+
+# variables numericas
+año = 2021
+estatura = 1.92
+
+
+print(type(nombre))
+print(type(año))
