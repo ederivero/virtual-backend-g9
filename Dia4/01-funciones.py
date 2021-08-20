f87b4b587b8711cd20bd0c47ca60eee44562b88f
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2021-08-19 22:01:10 -0500

    chore: fin funciones

--- Dia4/01-funciones.py
+++ Dia4/01-funciones.py
@@ -4,7 +4,7 @@ def saludar():
     print("Hola buenas tardes")
 
 
-saludar()
+# saludar()
 
 
 # funciones con parametros
@@ -14,7 +14,7 @@ def saludarPersona(nombre):
     print(f"Hola {nombre} como te va")
 
 
-saludarPersona("Eduardo")
+# saludarPersona("Eduardo")
 
 
 def sin_nombre():
@@ -22,7 +22,7 @@ def sin_nombre():
     print("Yo soy una funcion sin nombre")
 
 
-sin_nombre()
+# sin_nombre()
 
 
 # las funciones pueden recibir parametros y estos pueden ser opcionales
@@ -30,9 +30,8 @@ def registro(nombre, correo=None):
     print("Registro exitoso")
 
 
-registro("Eduardo")
-registro("Eduardo", "ederiveroman@gmail.com")
-# registro()
+# registro("Eduardo")
+# registro("Eduardo", "ederiveroman@gmail.com")
 
 
 # Crear una funcion llamada identificacion en la cual se reciba el nombre, apellido y la nacionalidad del cliente, si en el caso no se pasa la nacionalidad entonces sera Peruano, imprimir el resultado en forma de un diccionario
@@ -45,7 +44,7 @@ def identificacion(nombre, apellido, nacionalidad="Peruano"):
     print(resultado)
 
 
-identificacion("eduardo", "de rivero", "uruguayo")
+# identificacion("eduardo", "de rivero", "uruguayo")
 
 
 # todos los parametros que tengan un valor predeterminado SIEMPRE van al final
@@ -53,7 +52,7 @@ def sumatoria(num1, num2=10, num3=15):
     print(num1+num2+num3)
 
 
-sumatoria(10)
+# sumatoria(10)
 
 
 # el parametro que tiene el simbolo * es un parametro especial de python que sirve para almacenar n valores
@@ -62,32 +61,66 @@ def alumnos(*args):
     print(args)
 
 
-alumnos(
-    "Eduardo",
-    "Siannet",
-    "Pablo",
-    "Fernando",
-    "Rick",
-    "Jhonathan")
+# alumnos(
+#     "Eduardo",
+#     "Siannet",
+#     "Pablo",
+#     "Fernando",
+#     "Rick",
+#     "Jhonathan")
 
 
 def tareas(nombre, apellido, *args):
     print("ok")
 
 
-tareas("Eduardo", "martinez", "1", "2", 3)
+# tareas("Eduardo", "martinez", "1", "2", 3)
 
 
 # en la funcion alumnos_notas se recibira una cantidad N de alumnos en la cual se debe indicar cuantos aprobaron y cuantos desaprobaron siendo la nota minima 11
-def alumnos_notas():
+def alumnos_notas(*args):
     # todo: implementar logica
-    pass
+    aprobados = 0
+    desaprobados = 0
+    for alumno in args:
+        if alumno['promedio'] > 10:
+            aprobados += 1
+        else:
+            desaprobados += 1
+    print(
+        f"Hay {aprobados} alumnos aprobados y {desaprobados} alumnos desaprobados")
 
 
-alumnos_notas({
-    "nombre": "Raul", "promedio": 17,
-    "nombre": "Roxana", "promedio": 20,
-    "nombre": "Alfonso", "promedio": 10,
-    "nombre": "Pedro", "promedio": 8,
-    "nombre": "Katherine", "promedio": 16
-})
+alumnos_notas(
+    {"nombre": "Raul", "promedio": 17},
+    {"nombre": "Roxana", "promedio": 20},
+    {"nombre": "Alfonso", "promedio": 10},
+    {"nombre": "Pedro", "promedio": 8},
+    {"nombre": "Katherine", "promedio": 16}
+)
+
+
+# keyword arguments => es muy similar a los *args solo con la diferencia que los kwargs usan el nombre del parametro (nombre="Eduardo"), el resultado se guardara en forma de un diccionario
+def indeterminada(**kwargs):
+    print(kwargs)
+
+
+indeterminada(nombre="eduardo", apellido="de rivero", nacionalidad="Peruano")
+indeterminada(edad=50, estatura=2.10)
+
+
+def variada(*args, **kwargs):
+    print(args)
+    print(kwargs)
+
+
+variada(10, "Eduardo", {"est_civil": "Viudo"},
+        mascota="Firulais", raza="Bulldog")
+
+
+def sumatoria(num1, num2):
+    return num1+num2
+    print("otra cosa")
+
+
+rpta = sumatoria(10, 5)
