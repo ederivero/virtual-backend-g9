d089a5dc70b255f5df03480f668715fc2a2a03ab
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2021-08-23 22:02:05 -0500

    chore: avance flask

--- Dia1/api-flask.py
+++ Dia1/api-flask.py
@@ -2,7 +2,13 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-productos = []
+productos = [{
+    "nombre": "Palta fuerte",
+    "precio": 5.20
+}, {
+    "nombre": "Albahca 100gr.",
+    "precio": 0.80
+}]
 
 
 @app.route("/")
@@ -14,16 +20,45 @@ def inicio():
     }
 
 
-@app.route("/productos", methods=['POST'])
+@app.route("/productos", methods=['POST', 'GET'])
 def gestion_productos():
-    # el get_json() sirve para visualizar toda la informacion que el usuario me esta enviando por el body
-    # body es el cuerpo de la peticion (donde el front adjunta la informacion que quiere enviar)
-    # body se envia en formato JSON | multipart | TEXT | XML
-    print(request.get_json())
-    return {
-        "message": "Producto Creado exitosamente",
-        "content": ""
-    }
+
+    print(request.method)
+    if request.method == 'GET':
+        return {
+            "message": "",
+            "content": productos
+        }
+
+    elif request.method == 'POST':
+        # el get_json() sirve para visualizar toda la informacion que el usuario me esta enviando por el body
+        # body es el cuerpo de la peticion (donde el front adjunta la informacion que quiere enviar)
+        # body se envia en formato JSON | multipart | TEXT | XML
+        producto = request.get_json()
+        productos.append(producto)
+
+        return {
+            "message": "Producto Creado exitosamente",
+            "content": producto
+        }
+
+
+@app.route("/producto/<int:id>", methods=['GET'])
+def gestion_producto(id):
+    # buscar la posicion en la lista
+    # si no existe indicar que no existe
+    # si existe , mostrar el producto
+    total_productos = len(productos)
+    if id < total_productos:
+        return {
+            "content": productos[id],
+            "message": None
+        }
+    else:
+        return {
+            "message": "Producto no encontrado",
+            "content": None
+        }
 
 
 if __name__ == "__main__":
