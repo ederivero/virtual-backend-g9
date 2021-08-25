5716f5cc44eff7a24b0be39fc0598f2c361974f1
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2021-08-24 19:24:29 -0500

    avance front

--- Dia1/frontend/front.js
+++ Dia1/frontend/front.js
@@ -1,6 +1,26 @@
 console.log("Hola");
 const BASE_URL = "http://127.0.0.1:8000";
+const btnProductos = document.getElementById("btn-traer-productos");
 
 fetch(BASE_URL + "/", { method: "GET" }).then((respuesta) => {
   console.log(respuesta.status); // 200
+});
+
+btnProductos.onclick = (e) => {
+  // agregar codigo para solicitar los productos
+  fetch(BASE_URL + "/productos", { method: "GET" })
+    .then((respuesta) => {
+      return respuesta.json();
+    })
+    .then((productos) => {
+      console.log(productos);
+    });
+};
+
+btnProductos.addEventListener("click", async () => {
+  console.log("Me hizo click 2");
+  const respuesta = await fetch(BASE_URL + "/productos", { method: "GET" });
+  const productos = await respuesta.json();
+
+  console.log(productos);
 });
