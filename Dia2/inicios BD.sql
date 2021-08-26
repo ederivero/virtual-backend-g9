3cf302974d85b46c086961982623d5b15b9dbe15
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2021-08-25 22:08:32 -0500

    avance inicios bd

--- Dia2/inicios BD.sql
+++ Dia2/inicios BD.sql
@@ -112,3 +112,49 @@ DELETE FROM personas
 WHERE correo = 'mperez@gmail.com';
 SELECT *
 from personas;
+-- ACTUALIZAR UNO O VARIOS REGISTROS
+UPDATE personas
+SET nombre = 'RAMIRO'
+WHERE id = 1;
+CREATE TABLE medicos(
+    id int primary key not null auto_increment,
+    cmp varchar(5) unique not null,
+    -- colegio medico del peru
+    nombre varchar(30),
+    apellido varchar(50)
+);
+CREATE TABLE historial_vacunaciones(
+    id int primary key not null auto_increment,
+    vacuna enum('PFIZER', 'SINOPHARM', 'ASTRAZENECA'),
+    lote varchar(10),
+    fecha date,
+    medico_id int,
+    paciente_id int,
+    -- para crear las relaciones
+    foreign key (medico_id) references medicos(id),
+    foreign key (paciente_id) references personas(id)
+);
+INSERT INTO medicos (cmp, nombre, apellido)
+VALUES ('1234', 'RAUL', 'MUÑOZ'),
+    ('1236', 'ROSA', 'HIDALGO'),
+    ('5829', 'SOFIA', 'ALEMAN');
+select *
+from pruebas.personas;
+INSERT INTO historial_vacunaciones (vacuna, lote, medico_id, paciente_id, fecha)
+VALUES ('PFIZER', '1234', 1, 1, '2021-07-24'),
+    ('SINOPHARM', '1598', 2, 2, '2021-08-01'),
+    ('ASTRAZENECA', '1959', 1, 5, '2021-08-24'),
+    ('PFIZER', '1234', 1, 1, now()),
+    ('SINOPHARM', '5948', 3, 2, now());
+SELECT *
+FROM historial_vacunaciones;
+-- DEVOLVER TODOS LOS REGISTROS DE HISTORIALES DE VACUNACION QUE SEAN PFIZER O ASTRAZENECA
+SELECT *
+FROM historial_vacunaciones
+WHERE vacuna = 'PFIZER'
+    OR vacuna = 'ASTRAZENECA';
+SELECT *
+FROM historial_vacunaciones
+WHERE vacuna IN ('PFIZER', 'ASTRAZENECA');
+-- DELETE FROM HISTORIAL_VACUNACIONES;
+-- set SQL_SAFE_UPDATES = 1;
