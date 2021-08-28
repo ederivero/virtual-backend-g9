2563555b50e8d8a0a51cebb4faa69cfa7c501b0b
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2021-08-28 13:09:19 -0500

    avance flask bd

--- Dia5/CLAUSULA GROUP BY ORDER BY.sql
+++ Dia5/CLAUSULA GROUP BY ORDER BY.sql
@@ -32,12 +32,43 @@ group by nombre
 order by total desc, nombre asc;
 
 -- SELECT COLUMNAS | *
--- [WHERE CONDICION]
 -- FROM TABLAS [JOIN]
+-- [WHERE CONDICION]
 -- [GROUP BY AGRUPAMIENTOS]
--- [ORDER BY COL ASC | DESC];
+-- [ORDER BY COL ASC | DESC]
+-- LIMIT 0
+-- OFFSET 0;
 
+use calificaciones;
 -- Traer 
 -- NOMBRE_ALUMNO | APELLIDO_ALUMNO | NOMBRE_CURSO | ESTADO_CURSO
+SELECT alumnos.nombre as NombreAlumno, alumnos.apellido as ApellidoAlumno, 
+		cursos.nombre as NombreCurso, cursos.estado as EstadoCurso
+FROM ALUMNOS
+INNER JOIN ALUMNOS_CURSOS ON alumnos.id = alumnos_cursos.id_alumno
+INNER JOIN cursos ON cursos.id = alumnos_cursos.id_curso;
+
+-- SABER CUANTOS ALUMNOS ESTAN INSCRITOS EN CADA CURSO
+
+select * from cursos;
+-- que este ordenado de mas a menos alumnos inscritos y si es el mismo numero
+-- de alumnos inscritos que me de los cursos ordenados alfabeticamente
+-- CURSO_NOMBRE | ALUMNOS_INSCRITOS 
+SELECT count(id_alumno) as ALUMNOS_INSCRITOS, nombre as CURSO_NOMBRE
+from alumnos_cursos INNER JOIN cursos 
+	ON alumnos_cursos.id_curso = cursos.id
+group by nombre
+order by 1 desc, 2 asc;
+
+-- para el order by se puede poner los siguiente valores en relacion a los nombres
+-- de las columnas
+-- nombre original | alias | numero de la columna empezando en 1
+
+
+
+
+
+
+
 
 
--- Dia6/flask_con_bd.py
+++ Dia6/flask_con_bd.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -6,18 +6,43 @@ app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'root'
 app.config['MYSQL_DB'] = 'empresa'
-app.config['MYSQL_PORT'] = '3306'
+app.config['MYSQL_PORT'] = 3306
 
 print(app.config)
 mysql = MySQL(app)
 
 
-@app.route('/')
+@app.route('/departamentos', methods=['GET', 'POST'])
 def inicio():
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM DEPARTAMENTOS")
+    if request.method == 'GET':
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM DEPARTAMENTOS")
+        resultado = cur.fetchall()
+        print(resultado)
+        departamentos = []
+        for departamento in resultado:
+            print(departamento)
+            departamentos.append({
+                "id": departamento[0],
+                "nombre": departamento[1]
+            })
+        return {
+            "message": None,
+            "content": departamentos
+        }
+    elif request.method == 'POST':
+        # CAPTURAR EL BODY
+        data = request.get_json()
+        print(data)
+        cur = mysql.connection.cursor()
+        # %s => convierte el valor actual a string
+        cur.execute("INSERT INTO DEPARTAMENTOS (NOMBRE) VALUES ('%s')" %
+                    data['nombre'])
+        mysql.connection.commit()
 
-    return 'Bienvenido a mi api'
+        return {
+            "message": "Departamento creado exitosamente"
+        }, 201
 
 
 if __name__ == '__main__':
