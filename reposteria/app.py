f4fdbcce2fc6a003f2a1e6e4314b3e5737b90c35
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2021-08-30 20:23:14 -0500

    inicio proyecto flask orm

---
+++ reposteria/app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from conexion_bd import base_de_datos
+
+app = Flask(__name__)
+#                                        mysql://username:password@host/db_name
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/reposteria_flask'
+# si se establece True SqlAchemy rastreara las modificaciones de los objetos (modelos) y lanzara señales de cambio, su valor predeterminado es None . igual habilita el tracking pero emite una advertencia que en futuras versiones se removera el valor x default None y si o si tendremos que indicar un valor inicial
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# inicia la conexion con la bd para darle las credenciales definidias en el app.config
+base_de_datos.init_app(app)
+
+# creara las tablas aun no mapeadas y si todo esta bien no devolvera nada
+base_de_datos.create_all(app=app)
+
+
+@app.route("/")
+def initial_controller():
+    return {
+        "message": "Bienvenido a mi API de REPOSTERIA 🥧"
+    }
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
