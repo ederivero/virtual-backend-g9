4669ce31b6ba93dda81bc019312a4d5e9e790846
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2021-09-20 22:02:16 -0500

    chore: avance modelado django

---
+++ libreria/gestion/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+# https://docs.djangoproject.com/en/3.2/ref/models/
+# https://docs.djangoproject.com/en/3.2/topics/db/models/
+class ProductoModel(models.Model):
+    # Tipos de datos del ORM => https://docs.djangoproject.com/en/3.2/ref/models/fields/#field-types
+    # Parametros genericos de los tipos de datos => https://docs.djangoproject.com/en/3.2/ref/models/fields/#field-options
+    productoId = models.AutoField(
+        primary_key=True, null=False, unique=True, db_column='id')
+
+    productoNombre = models.CharField(
+        max_length=45, db_column='nombre', null=False)
