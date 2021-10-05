5d707c16f58eeb656a6601df4ea2a14024676b66
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2021-10-05 18:28:54 -0500

    avance

--- facturacion/views.py
+++ facturacion/views.py
@@ -1,6 +1,7 @@
 from rest_framework.generics import ListCreateAPIView
 from .serializers import ComprobanteSerializer
 from rest_framework.response import Response
+from .generarComprobante import crearComprobante
 
 
 class ComprobanteController(ListCreateAPIView):
@@ -10,6 +11,13 @@ class ComprobanteController(ListCreateAPIView):
         data = self.serializer_class(data=request.data)
         if data.is_valid():
             print(data.validated_data)
+            tipoComprobante = data.validated_data.get('tipoComprobante')
+            pedidoId = data.validated_data.get('pedidoId')
+            numeroDocumento = data.validated_data.get('numeroDocumento')
+
+            tipoComprobante = 2 if tipoComprobante == 'BOLETA' else 1
+
+            crearComprobante(tipoComprobante, pedidoId, numeroDocumento)
             return Response(data={
                 'message': ''
             }, status=201)
