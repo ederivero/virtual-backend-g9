cbe93a5ae65b9d82c62f876d8245344c5339875a
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2021-10-06 19:19:10 -0500

    chore: avance express

--- actividades/src/config/server.js
+++ actividades/src/config/server.js
@@ -1,10 +1,23 @@
 import express from "express";
-
+import morgan from "morgan";
 export class Server {
   constructor() {
     this.app = express();
     this.puerto = 8000;
+    this.rutas();
   }
+
+  rutas() {
+    // agregamos el middleware de morgan para haga tracking a todas las consultas al backend
+    this.app.use(morgan("dev"));
+
+    this.app.get("/", (req, res) => {
+      // req => Request => la informacion que me enviar el cliente
+      // res => Response => la informacion que le voy a devolver al cliente
+      res.send("Bienvenido a mi API");
+    });
+  }
+
   start() {
     this.app.listen(this.puerto, () => {
       // alt+96 ``
