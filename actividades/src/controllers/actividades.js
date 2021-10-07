e44b0dc42d42a89482f1c86d9ca0153271d75590
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2021-10-06 22:03:04 -0500

    chore: avance express

--- actividades/src/controllers/actividades.js
+++ actividades/src/controllers/actividades.js
@@ -1,6 +1,18 @@
 // CRUD ACTIVIDADES
-const actividades = [];
+const actividades = [
+  {
+    nombre: "Ir al gimnasio",
+    hora: "6:00",
+    dias: ["LUN", "MIE", "VIE"],
+  },
+  {
+    nombre: "Aprender MongoDb",
+    hora: "22:00",
+    dias: ["MAR", "SAB"],
+  },
+];
 
+// Documentacion del request http://expressjs.com/en/5x/api.html#req
 export const crearActividad = (req, res) => {
   console.log(req.body); // me dara todo el body enviado por el usuario
   const { body } = req;
@@ -17,4 +29,42 @@ export const listarActividades = (req, res) => {
     message: "Las actividades son:",
     content: actividades,
   });
+};
+
+export const devolverActividad = (req, res) => {
+  console.log(req.params);
+  const { id } = req.params;
+  // buscar en el array esa posicion y si no hay retornar una res con estado 404
+  //{
+  //   message: 'Actividad no encontrada'
+  //}
+  // y si si existe entonces retornar esa actividad
+  if (actividades.length > id) {
+    return res.json({
+      message: null,
+      content: actividades[id],
+    });
+  } else {
+    return res.status(404).json({
+      message: "Actividad no encontrada",
+      content: null,
+    });
+  }
+};
+
+export const actualizarActividad = (req, res) => {
+  const { id } = req.params;
+  if (actividades.length > id) {
+    actividades[id] = req.body;
+
+    return res.json({
+      message: "actividad actualizada exitosamente",
+      content: actividades[id],
+    });
+  } else {
+    return res.status(404).json({
+      message: "No se encontro la actividad a actualizar",
+      content: null,
+    });
+  }
 };
--- actividades/src/routes/actividades.js
+++ actividades/src/routes/actividades.js
@@ -1,6 +1,17 @@
 import { Router } from "express";
-import { crearActividad, listarActividades } from "../controllers/actividades";
+import {
+  crearActividad,
+  listarActividades,
+  devolverActividad,
+  actualizarActividad,
+} from "../controllers/actividades";
 
 export const actividades_router = Router();
 actividades_router.post("/actividad", crearActividad);
 actividades_router.get("/actividades", listarActividades);
+// actividades_router.get("/actividad/:id", devolverActividad);
+// actividades_router.put("/actividad/:id", actualizarActividad);
+actividades_router
+  .route("/actividad/:id")
+  .get(devolverActividad)
+  .put(actualizarActividad);
