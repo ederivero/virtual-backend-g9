234c5e143896bfbf5e210ec1c0dcc56ff321e444
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2021-10-12 19:15:27 -0500

    modifique conexion bd

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,6 +1,7 @@
 import { Sequelize } from "sequelize";
+require("dotenv").config();
 
 export const conexion = new Sequelize(
-  "postgresql://postgres:root@localhost:5432/tareas",
+  process.env.DATABASE_URL,
   { logging: false } // logging => indica si que quiere mostrar o no las consultas a la bd
 );
