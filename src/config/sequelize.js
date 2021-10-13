fbb46bc5f015e93bb7616fbfbba9f0e9b6215ccf
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2021-10-12 19:28:55 -0500

    modificacion VAR DB

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -2,6 +2,6 @@ import { Sequelize } from "sequelize";
 require("dotenv").config();
 
 export const conexion = new Sequelize(
-  process.env.DATABASE_URL,
+  process.env.DATABASE_URL2,
   { logging: false } // logging => indica si que quiere mostrar o no las consultas a la bd
 );
