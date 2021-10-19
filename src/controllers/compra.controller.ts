f0f48bfb7d64718c92a158882c1cf1011788f5a9
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2021-10-18 19:34:21 -0500

    avance transacciones

--- src/controllers/compra.controller.ts
+++ src/controllers/compra.controller.ts
@@ -16,16 +16,32 @@ export const crearCompra = async (req: RequestUser, res: Response) => {
     //   console.log(errores[0].children[0].children);
     // }
 
-    const mensaje_error = errores.map((error) => error.constraints);
+    // ingreso a las constraints en primer nivel
+    const mensaje_error = errores.map((error) => error.children);
+    // ingresar en un segundo nivel y mapear esos errores nuevamente (ya que tenemos un DTO dentro de otro)
+    const mensaje_error_final = mensaje_error.map((error) =>
+      error?.map((error_hijo) => {
+        console.log(error_hijo);
+        // ingresaba a los errores del dto adyacente e ingresaba nuevamente a sus errores para ahora si devolver el constraint
+
+        return error_hijo.children
+          ? error_hijo.children.map(
+              (error_hijo_hijo) => error_hijo_hijo.constraints
+            )
+          : null;
+      })
+    );
+    console.log(mensaje_error);
 
     return res.status(400).json({
-      content: mensaje_error,
+      content: mensaje_error_final,
       message: "Campos invalidos",
     });
   }
 
   const trasaccion = await conexion.transaction();
   try {
+    // 1. Creo la compra (cabecera de la compra)
     const nuevaCompra = await Compras.create(
       {
         compraFecha: new Date(),
@@ -35,32 +51,62 @@ export const crearCompra = async (req: RequestUser, res: Response) => {
       { transaction: trasaccion }
     );
 
-    validador.detalle.map(async (detalle_compra) => {
-      const producto = await Productos.findByPk(detalle_compra.producto, {
-        attributes: ["productoCantidad", "productoPrecio"],
-      });
-      if (!producto) {
-        throw new Error(`Producto ${detalle_compra.producto} no existe`);
-      }
-      // ahora validamos si hay la cantidad suficiente
-      if (detalle_compra.cantidad > producto.getDataValue("productoCantidad")) {
-        throw new Error(
-          `Producto ${detalle_compra.producto} no hay suficiente cantidades`
+    // asi seria el uso sin la necesitad de un Promise.all y con un forin sencillo
+    for (const key in validador.detalle) {
+    }
+    // asi seria el uso con la espera de un Promise.all (espera la ejecuion de todas las promesas)
+    await Promise.all(
+      validador.detalle.map(async (detalle_compra) => {
+        const producto = await Productos.findByPk(detalle_compra.producto, {
+          attributes: ["productoCantidad", "productoPrecio"],
+        });
+        if (!producto) {
+          throw new Error(`Producto ${detalle_compra.producto} no existe`);
+        }
+        // ahora validamos si hay la cantidad suficiente
+        if (
+          detalle_compra.cantidad > producto.getDataValue("productoCantidad")
+        ) {
+          throw new Error(
+            `Producto ${detalle_compra.producto} no hay suficiente cantidades`
+          );
+        }
+        // ahora creamos el detalle de ese item
+        await Detalles.create(
+          {
+            productoId: detalle_compra.producto,
+            compraId: nuevaCompra.getDataValue("compraId"),
+            detalleCantidad: detalle_compra.cantidad,
+            detalleTotal:
+              detalle_compra.cantidad * producto.getDataValue("productoPrecio"),
+          },
+          { transaction: trasaccion }
         );
-      }
-      // ahora creamos el detalle de ese item
-      await Detalles.create(
-        {
-          productoId: detalle_compra.producto,
-          compraId: nuevaCompra.getDataValue("compraId"),
-          detalleCantidad: detalle_compra.cantidad,
-          detalleTotal:
-            detalle_compra.cantidad * producto.getDataValue("productoPrecio"),
-        },
-        { transaction: trasaccion }
-      );
 
-      // disminuir la cantidad del item
+        // disminuir la cantidad del item
+        // UPDATE productos SET cantidad = cantidad - 1 WHERE id = '.....';
+        await Productos.update(
+          {
+            productoCantidad:
+              producto.getDataValue("productoCantidad") -
+              detalle_compra.cantidad,
+          },
+          {
+            where: {
+              productoId: detalle_compra.producto,
+            },
+            transaction: trasaccion,
+          }
+        );
+        const compra = await Compras.findByPk(nuevaCompra.getDataValue("id"));
+
+        // await Compras.update({compraTotal: compra?.getDataValue('compraTotal')})
+      })
+    );
+    await trasaccion.commit();
+    return res.status(201).json({
+      message: "Compra realizada exitosamente",
+      content: nuevaCompra,
     });
   } catch (error: unknown) {
     await trasaccion.rollback();
@@ -69,8 +115,4 @@ export const crearCompra = async (req: RequestUser, res: Response) => {
       content: error instanceof Error ? error.message : "",
     });
   }
-
-  return res.json({
-    message: "Compra creada exitosamente",
-  });
 };
--- src/routes/compra.routes.ts
+++ src/routes/compra.routes.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
 import * as compraController from "../controllers/compra.controller";
-
+import { authValidator } from "../middlewares/validator";
 const compraRouter = Router();
 
-compraRouter.post("/compra", compraController.crearCompra);
+compraRouter.post("/compra", authValidator, compraController.crearCompra);
 
 export default compraRouter;
