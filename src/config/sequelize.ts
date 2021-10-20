f0bfe13c9c00d7aeabeda2f2ba2baeaf4a0d9284
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2021-10-19 20:45:18 -0500

    agrege configuracion conexion bd

--- src/config/sequelize.ts
+++ src/config/sequelize.ts
@@ -2,5 +2,10 @@ import { Sequelize } from "sequelize";
 require("dotenv").config();
 
 export default new Sequelize(process.env.DATABASE_URL ?? "", {
+  dialectOptions:
+    process.env.NODE_ENV !== "production"
+      ? {}
+      : { ssl: { rejectUnauthorized: false } },
+
   logging: false,
 });
